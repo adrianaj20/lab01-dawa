3f1dbfde3b5f0549fcf6cfbb7179e35bf6c82cde
adrianaj20 <adrian.najarro@tecsup.edu.pe>
2023-03-23 22:59:42 -0500

    primer commit

---
+++ tarea1/pag.js
@@ -0,0 +1,27 @@
+var http = require('http'),
+fs = require('fs');
+
+http.createServer(function(solicitud,respuesta){
+
+    const { url } = solicitud;
+    if (url === '/') {
+        template='index.html'
+      }
+    else if(url === '/nosotros') {
+        template='nosotros.html'
+      }
+    else if(url === '/servicios') {
+        template='servicios.html'
+      }
+    else if(url === '/clientes') {
+        template='clientes.html'
+      }
+    else if(url === '/contacto') {
+        template='contacto.html'
+      }
+
+    fs.readFile('templates/'+template,function(error,html) {
+    respuesta.write(html);
+    respuesta.end();
+    });
+    }).listen(8080);
